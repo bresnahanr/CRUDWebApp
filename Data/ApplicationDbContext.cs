33d137fd8c663daf7e7c98fedfb2e0e8df785ba9
Ryan Bresnahan <r.bresnahan@hotmail.com>
2024-03-28 13:32:13 -0600

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CRUDWebAPP.Models;
+
+namespace CRUDWebAPP.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<CRUDWebAPP.Models.Joke> Joke { get; set; }
+	}
+}
